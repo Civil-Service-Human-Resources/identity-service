b4cae840f8b9f4e8627bc04f9a95ee6d0e2e8e31
Yuksel Kurtbas <36735726+elxsy@users.noreply.github.com>
2018-05-10 16:54:59 +0100

    LPFG-257 Remove truncation

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,3 @@
-SET FOREIGN_KEY_CHECKS = 0;
-
-TRUNCATE TABLE `client`;
-TRUNCATE TABLE `role`;
-TRUNCATE TABLE `identity`;
-TRUNCATE TABLE `identity_role`;
-
 INSERT INTO `client` (active, uid, password, redirect_uri)
 VALUES (true, '9fbd4ae2-2db3-44c7-9544-88e80255b56e', '$2a$10$AbxhLGtIx7yv8jhF0BePiOxnb1mlHHq/Ge4R3PxCL2wIsoEov1VaS', 'http://lpg.local.cshr.digital:3001/authenticate');
 
@@ -19,6 +12,3 @@ INSERT INTO `identity` (active, email, uid, password) VALUES
 INSERT INTO `identity_role` (identity_id, role_id) VALUES
 (SELECT id FROM identity WHERE email = 'user@domain.com', SELECT id FROM role WHERE name = 'USER'),
 (SELECT id FROM identity WHERE email = 'super@domain.com', SELECT id FROM role WHERE name = 'SUPER_USER');
-
-SET FOREIGN_KEY_CHECKS = 1;
-
