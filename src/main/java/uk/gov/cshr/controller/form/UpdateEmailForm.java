4446b0b6436d0e4d9e9ee5b9f38f2aca93b9b83d
Rob Leney <roble@kainos.com>
2019-01-17 14:48:41 +0000

    LPFG-1130: Added validator for email domain whitelist.

--- src/main/java/uk/gov/cshr/controller/form/UpdateEmailForm.java
+++ src/main/java/uk/gov/cshr/controller/form/UpdateEmailForm.java
@@ -2,6 +2,7 @@ package uk.gov.cshr.controller.form;
 
 import lombok.Data;
 import uk.gov.cshr.validation.annotation.FieldMatch;
+import uk.gov.cshr.validation.annotation.Whitelisted;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -11,6 +12,7 @@ import javax.validation.constraints.NotBlank;
 public class UpdateEmailForm {
     @NotBlank(message = "{validation.updateEmail.email.NotBlank}")
     @Email(message = "{validation.updateEmail.email.Email}")
+    @Whitelisted(message = "{validation.updateEmail.email.Whitelisted}")
     private String email;
 
     @NotBlank(message = "{validation.updateEmail.confirm.NotBlank}")
---
+++ src/test/java/uk/gov/cshr/validation/validator/WhitelistedValidatorTest.java
@@ -0,0 +1,28 @@
+package uk.gov.cshr.validation.validator;
+
+import org.junit.Test;
+
+import javax.validation.ConstraintValidatorContext;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+public class WhitelistedValidatorTest {
+    private String[] whitelistedDomains = {"example.org", "domain.org"};
+
+    private WhitelistedValidator validator = new WhitelistedValidator(whitelistedDomains);
+
+    private ConstraintValidatorContext constraintValidatorContext = mock(ConstraintValidatorContext.class);
+
+    @Test
+    public void shouldReturnTrueIfValueHasWhitelistedDomain() {
+        assertTrue(validator.isValid("user@domain.org", constraintValidatorContext));
+        assertTrue(validator.isValid("user@example.org", constraintValidatorContext));
+    }
+
+    @Test
+    public void shouldReturnFalseIfValueDoesNotHaveWhitelistedDomain() {
+        assertFalse(validator.isValid("user@not-in-whitelist.org", constraintValidatorContext));
+    }
+}
