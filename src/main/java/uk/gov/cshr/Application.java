7add8af4418f6585ba36e290054b776f2c4625d3
matthewmu <matthewmu@kainos.com>
2019-09-27 16:14:25 +0100

    fix: update token caching for tokens

--- src/main/java/uk/gov/cshr/Application.java
+++ src/main/java/uk/gov/cshr/Application.java
@@ -3,6 +3,7 @@ package uk.gov.cshr;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
@@ -12,6 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootApplication
+@EnableCaching
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/uk/gov/cshr/service/security/TokenStore.java
+++ src/main/java/uk/gov/cshr/service/security/TokenStore.java
@@ -1,5 +1,7 @@
 package uk.gov.cshr.service.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
@@ -20,6 +22,7 @@ import static uk.gov.cshr.domain.Token.extractTokenKey;
 
 @Service
 public class TokenStore implements org.springframework.security.oauth2.provider.token.TokenStore {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TokenStore.class);
 
     private AuthenticationKeyGenerator authenticationKeyGenerator = new DefaultAuthenticationKeyGenerator();
 
@@ -33,11 +36,13 @@ public class TokenStore implements org.springframework.security.oauth2.provider.
     @Override
     @Cacheable(cacheNames = "readAuthenticationCache", key = "#token.toString()")
     public OAuth2Authentication readAuthentication(OAuth2AccessToken token) {
+        LOGGER.info("1. readAuthentication");
         return readAuthentication(token.getValue());
     }
 
     @Override
     public OAuth2Authentication readAuthentication(String tokenValue) {
+        LOGGER.info("2. readAuthentication no cache");
         Token token = tokenRepository.findByTokenIdAndStatus(extractTokenKey(tokenValue), TokenStatus.ACTIVE);
         if (token != null) {
             return token.getAuthentication();
@@ -47,6 +52,7 @@ public class TokenStore implements org.springframework.security.oauth2.provider.
 
     @Override
     public void storeAccessToken(OAuth2AccessToken token, OAuth2Authentication authentication) {
+        LOGGER.info("3. storeAccessToken");
         Token storedToken = tokenRepository.findByTokenIdAndStatus(extractTokenKey(token.getValue()), TokenStatus.ACTIVE);
         if (storedToken == null) {
             storedToken = new Token(authenticationKeyGenerator.extractKey(authentication), token, authentication);
@@ -59,6 +65,8 @@ public class TokenStore implements org.springframework.security.oauth2.provider.
     @Override
     @Cacheable(cacheNames = "readAccessTokenCache", key = "#tokenValue")
     public OAuth2AccessToken readAccessToken(String tokenValue) {
+        LOGGER.info("4. readAccessToken");
+
         Token token = tokenRepository.findByTokenIdAndStatus(extractTokenKey(tokenValue), TokenStatus.ACTIVE);
         if (token != null) {
             return token.getToken();
@@ -69,6 +77,8 @@ public class TokenStore implements org.springframework.security.oauth2.provider.
     @Override
     @CacheEvict(cacheNames = "readAccessTokenCache", key = "#token.getValue()")
     public void removeAccessToken(OAuth2AccessToken token) {
+        LOGGER.info("5. removeAccessToken");
+
         Token storedToken = tokenRepository.findByTokenIdAndStatus(extractTokenKey(token.getValue()), TokenStatus.ACTIVE);
         if (storedToken != null) {
             storedToken.setStatus(TokenStatus.REVOKED);
@@ -78,6 +88,7 @@ public class TokenStore implements org.springframework.security.oauth2.provider.
 
     @Override
     public void storeRefreshToken(OAuth2RefreshToken refreshToken, OAuth2Authentication authentication) {
+        LOGGER.info("6. storeRefreshToken");
     }
 
     @Override
@@ -100,6 +111,8 @@ public class TokenStore implements org.springframework.security.oauth2.provider.
 
     @Override
     public OAuth2AccessToken getAccessToken(OAuth2Authentication authentication) {
+        LOGGER.info("7. getAccessToken");
+
         Token storedToken = tokenRepository.findByAuthenticationIdAndStatus(authenticationKeyGenerator.extractKey(authentication), TokenStatus.ACTIVE);
         if (storedToken != null) {
             return storedToken.getToken();
@@ -109,6 +122,8 @@ public class TokenStore implements org.springframework.security.oauth2.provider.
 
     @Override
     public Collection<OAuth2AccessToken> findTokensByClientIdAndUserName(String clientId, String userName) {
+        LOGGER.info("8. findTokensByClientIdAndUserName");
+
         Collection<Token> storedTokens = tokenRepository.findByClientIdAndUserName(clientId, userName);
         return storedTokens.stream()
                 .map(Token::getToken)
@@ -117,6 +132,8 @@ public class TokenStore implements org.springframework.security.oauth2.provider.
 
     @Override
     public Collection<OAuth2AccessToken> findTokensByClientId(String clientId) {
+        LOGGER.info("9. findTokensByClientId");
+
         Collection<Token> storedTokens = tokenRepository.findByClientId(clientId);
         return storedTokens.stream()
                 .map(Token::getToken)
