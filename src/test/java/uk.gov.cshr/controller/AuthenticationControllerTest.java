5afbadb195492a80f05a933ccdba31214597d6ca
jaylco <77386599+jaylco@users.noreply.github.com>
2023-11-02 14:37:13 +0000

    no-redis profile now that caching is enabled

--- src/test/java/uk.gov.cshr/controller/AuthenticationControllerTest.java
+++ src/test/java/uk.gov.cshr/controller/AuthenticationControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest(classes = Application.class)
+@ActiveProfiles("no-redis")
 public class AuthenticationControllerTest {
 
     @Autowired
