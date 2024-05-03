ab19437bcf998a819fa39e6ae8cc941a9bdcbda8
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2024-05-03 20:27:06 +0100

    LC-2959-Bypass maintenance page for the defined users to test the release

--- src/main/java/uk/gov/cshr/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/uk/gov/cshr/config/CustomAuthenticationSuccessHandler.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import uk.gov.cshr.domain.Identity;
 import uk.gov.cshr.service.security.IdentityDetails;
 
@@ -15,10 +15,10 @@ import java.util.Arrays;
 
 @Slf4j
 @Configuration
-public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+public class CustomAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
     @Value("${lpg.uiUrl}")
-    private String lpgUiSignOutUrl;
+    private String lpgUiUrl;
 
     @Value("${maintenancePage.enabled}")
     private boolean maintenancePageEnabled;
@@ -30,6 +30,7 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) {
+        this.setDefaultTargetUrl(lpgUiUrl);
         if(maintenancePageEnabled) {
             Object principal = authentication.getPrincipal();
             if(principal instanceof IdentityDetails) {
@@ -41,11 +42,11 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
                 if(skipMaintenancePage) {
                     log.info("Maintenance page is skipped for the user: {}", username);
                 } else {
-                    log.info("Trying to logout the user to Display Maintenance page for the user: {}", username);
-                    response.sendRedirect(lpgUiSignOutUrl+"/sign-out");
-                    log.info("Redirecting the user {} to lpg-ui/sign-out url to logout.", username);
+                    log.info("Trying to logout the user by redirecting to lpg-ui/sign-out to Display Maintenance page for the user: {}", username);
+                    response.sendRedirect(lpgUiUrl+"/sign-out");
                 }
             }
         }
+        super.onAuthenticationSuccess(request, response, authentication);
     }
 }
