49b8ab68a9ec90f26633cac14afa59edb46d584d
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2024-05-10 12:40:06 +0100

    LC-2959-Bypass maintenance page for the defined users to test the release

--- src/test/java/uk/gov/cshr/utils/MaintenancePageUtilTest.java
+++ src/test/java/uk/gov/cshr/utils/MaintenancePageUtilTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.core.Authentication;
 import org.springframework.ui.Model;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +17,6 @@ import static org.mockito.Mockito.*;
 public class MaintenancePageUtilTest {
 
     private static final String SKIP_MAINTENANCE_PAGE_PARAM_NAME = "username";
-    private final String username = "tester1@domain.com";
     private final String skipMaintenancePageForUsers = "tester1@domain.com,tester2@domain.com";
     private final String maintenancePageContentLine1 = "The learning website is undergoing scheduled maintenance.";
     private final String maintenancePageContentLine2 = "It will be unavailable between the hours of 7pm to 9pm on Wednesday 24th February 2021.";
@@ -29,8 +29,11 @@ public class MaintenancePageUtilTest {
     @Mock
     private HttpServletRequest request;
 
+    @Mock
+    private Authentication authentication;
+
     @Test
-    public void shouldNotDisplayMaintenancePageIfDisabled() {
+    public void shouldNotDisplayMaintenancePageIfMaintenancePageIsDisabled() {
         MaintenancePageUtil maintenancePageUtil = new MaintenancePageUtil(false,
                 skipMaintenancePageForUsers, maintenancePageContentLine1, maintenancePageContentLine2,
                 maintenancePageContentLine3,maintenancePageContentLine4);
@@ -44,12 +47,44 @@ public class MaintenancePageUtilTest {
     }
 
     @Test
-    public void shouldNotDisplayMaintenancePageIfEnabledButSkippedForUser() {
+    public void shouldDisplayMaintenancePageIfMaintenancePageIsEnabledAndUsernameIsNotPassedInRequestParam() {
         MaintenancePageUtil maintenancePageUtil = new MaintenancePageUtil(true,
                 skipMaintenancePageForUsers, maintenancePageContentLine1, maintenancePageContentLine2,
                 maintenancePageContentLine3,maintenancePageContentLine4);
 
-        when(request.getParameter("username")).thenReturn(username);
+        when(request.getParameter("username")).thenReturn(null);
+
+        assertTrue(maintenancePageUtil.displayMaintenancePage(request, model));
+        verify(request, times(1)).getParameter(SKIP_MAINTENANCE_PAGE_PARAM_NAME);
+        verify(model, times(1)).addAttribute("maintenancePageContentLine1", maintenancePageContentLine1);
+        verify(model, times(1)).addAttribute("maintenancePageContentLine2", maintenancePageContentLine2);
+        verify(model, times(1)).addAttribute("maintenancePageContentLine3", maintenancePageContentLine3);
+        verify(model, times(1)).addAttribute("maintenancePageContentLine4", maintenancePageContentLine4);
+    }
+
+    @Test
+    public void shouldDisplayMaintenancePageIfMaintenancePageIsEnabledAndUsernamePassedInRequestParamIsNotAllowedToSkipMaintenancePage() {
+        MaintenancePageUtil maintenancePageUtil = new MaintenancePageUtil(true,
+                skipMaintenancePageForUsers, maintenancePageContentLine1, maintenancePageContentLine2,
+                maintenancePageContentLine3,maintenancePageContentLine4);
+
+        when(request.getParameter("username")).thenReturn("tester3@domain.com");
+
+        assertTrue(maintenancePageUtil.displayMaintenancePage(request, model));
+        verify(request, times(1)).getParameter(SKIP_MAINTENANCE_PAGE_PARAM_NAME);
+        verify(model, times(1)).addAttribute("maintenancePageContentLine1", maintenancePageContentLine1);
+        verify(model, times(1)).addAttribute("maintenancePageContentLine2", maintenancePageContentLine2);
+        verify(model, times(1)).addAttribute("maintenancePageContentLine3", maintenancePageContentLine3);
+        verify(model, times(1)).addAttribute("maintenancePageContentLine4", maintenancePageContentLine4);
+    }
+
+    @Test
+    public void shouldNotDisplayMaintenancePageIfMaintenancePageIsEnabledAndUsernamePassedInRequestParamIsAllowedToSkipMaintenancePage() {
+        MaintenancePageUtil maintenancePageUtil = new MaintenancePageUtil(true,
+                skipMaintenancePageForUsers, maintenancePageContentLine1, maintenancePageContentLine2,
+                maintenancePageContentLine3,maintenancePageContentLine4);
+
+        when(request.getParameter("username")).thenReturn("tester1@domain.com");
 
         assertFalse(maintenancePageUtil.displayMaintenancePage(request, model));
         verify(request, times(1)).getParameter(SKIP_MAINTENANCE_PAGE_PARAM_NAME);
@@ -60,16 +95,16 @@ public class MaintenancePageUtilTest {
     }
 
     @Test
-    public void shouldDisplayMaintenancePageIfEnabled() {
-        MaintenancePageUtil maintenancePageUtil = new MaintenancePageUtil(true,
+    public void shouldSkipMaintenancePageForLoggedInUserIfMaintenancePageIsDisabled() {
+        MaintenancePageUtil maintenancePageUtil = new MaintenancePageUtil(false,
                 skipMaintenancePageForUsers, maintenancePageContentLine1, maintenancePageContentLine2,
                 maintenancePageContentLine3,maintenancePageContentLine4);
 
-        assertTrue(maintenancePageUtil.displayMaintenancePage(request, model));
-        verify(request, times(1)).getParameter(SKIP_MAINTENANCE_PAGE_PARAM_NAME);
-        verify(model, times(1)).addAttribute("maintenancePageContentLine1", maintenancePageContentLine1);
-        verify(model, times(1)).addAttribute("maintenancePageContentLine2", maintenancePageContentLine2);
-        verify(model, times(1)).addAttribute("maintenancePageContentLine3", maintenancePageContentLine3);
-        verify(model, times(1)).addAttribute("maintenancePageContentLine4", maintenancePageContentLine4);
+        maintenancePageUtil.skipMaintenancePageCheck(authentication);
+
+        verify(model, times(0)).addAttribute("maintenancePageContentLine1", maintenancePageContentLine1);
+        verify(model, times(0)).addAttribute("maintenancePageContentLine2", maintenancePageContentLine2);
+        verify(model, times(0)).addAttribute("maintenancePageContentLine3", maintenancePageContentLine3);
+        verify(model, times(0)).addAttribute("maintenancePageContentLine4", maintenancePageContentLine4);
     }
 }
